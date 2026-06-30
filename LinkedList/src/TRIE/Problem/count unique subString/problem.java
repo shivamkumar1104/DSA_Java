917d69120ce5a34edc1349d69e362bcbb941b9a4
shivamkumar1104 <shivamkumaar2004@gmail.com>
2026-06-30 13:59:56 +0530

    Auto-commit: update DSA practice

--- LinkedList/src/TRIE/Problem/count unique subString/problem.java
+++ LinkedList/src/TRIE/Problem/count unique subString/problem.java
@@ -1,9 +1,25 @@
 public class problem {
     
-        
     
+    static class Node{
+        Node[] children;
+        boolean eow;
+
+        public Node(){
+            children = new Node[26];
+            for(int i = 0; i < 26; i++){
+                children[i] = null;
+
+            }
+            eow = false;
+
+        }
+    }
+
     public static void main(String[] args) {
-        string words[] = {};
+        String s = "ababa";
+        
+        
     }
 
 }
