20ddbc73ddd3e8ce94ac4ebc85c52be646b21cf2
shivamkumar1104 <shivamkumaar2004@gmail.com>
2026-08-10 21:56:16 +0530

    Auto-commit: update DSA practice

--- LinkedList/src/DP/problems/climbChair.java
+++ LinkedList/src/DP/problems/climbChair.java
@@ -1,8 +1,13 @@
 import java.util.*;
 
 public class climbChair {
-     
+     public static int climb(int n){
+        if(n == 0 || n == 1) return 1;
+        if(n < 0) return 0;
+        return 
+     }
     public static void main(String[] args) {
+        
         
     }
 }
